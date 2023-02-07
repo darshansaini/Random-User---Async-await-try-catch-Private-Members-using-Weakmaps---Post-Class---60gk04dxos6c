01613f728f8702a9c14a0c6d4cef1cd18b8ce61a
Darshan Saini <50915527+darshansaini@users.noreply.github.com>
2023-02-07 15:18:18 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,137 +1,83 @@
 import React from 'react'
 import '../styles/App.css';
+import { useEffect, useState } from "react"
 
-
-
-// Sample response from api below, dont use this data
-// {
-//   results: [
-//     {
-//       gender: "female",
-//       name: {
-//         title: "Miss",
-//         first: "Zara",
-//         last: "White"
-//       },
-//       location: {
-//         street: {
-//           number: 9548,
-//           name: "Wairau Road"
-//         },
-//         city: "Hamilton",
-//         state: "Tasman",
-//         country: "New Zealand",
-//         postcode: 52652,
-//         coordinates: {
-//           latitude: "68.0268",
-//           longitude: "114.0576"
-//         },
-//         timezone: {
-//           offset: "-1:00",
-//           description: "Azores, Cape Verde Islands"
-//         }
-//       },
-//       email: "zara.white@example.com",
-//       login: {
-//         uuid: "fd26ff4c-794a-41d9-bc82-c79997d6309b",
-//         username: "bigrabbit521",
-//         password: "liang",
-//         salt: "JsOd1LTX",
-//         md5: "c60353ef1d02626f8afcb50bc80baac2",
-//         sha1: "2a0aea755e22c24f52a75b6b972af4e32a892997",
-//         sha256: "f01f1d678bebaec80d452621a9a81f78296079a0c3fdb9bea24d1a74561863d2"
-//       },
-//       dob: {
-//         date: "1978-03-29T10:36:08.698Z",
-//         age: 44
-//       },
-//       registered: {
-//         date: "2012-09-08T19:40:27.630Z",
-//         age: 10
-//       },
-//       phone: "(483)-206-7882",
-//       cell: "(986)-684-2134",
-//       id: {
-//         name: "",
-//         value: null
-//       },
-//       picture: {
-//         large: "https://randomuser.me/api/portraits/women/60.jpg",
-//         medium: "https://randomuser.me/api/portraits/med/women/60.jpg",
-//         thumbnail: "https://randomuser.me/api/portraits/thumb/women/60.jpg"
-//       },
-//       nat: "NZ"
-//     }
-//   ],
-//     info: {
-//     seed: "00bf0e8b7e323357",
-//       results: 1,
-//         page: 1,
-//           version: "1.3"
-//   }
-// }
 const App = () => {
-  
-const imageElement = document.getElementById('image');
-const nameElement = document.getElementById('name');
-const ageButton = document.getElementById('age');
-const emailButton = document.getElementById('email');
-const phoneButton = document.getElementById('phone');
-const additionalInfoElement = document.getElementById('additional-info');
-const getUserButton = document.getElementById('getUser');
+    const [users, setUsers] = useState([])
+    const [age, setAge] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+    const [state1, setState1] = useState(false);
+    const [state2, setState2] = useState(false);
+    const [state3, setState3] = useState(false);
+    const [name, setName] = useState("");
+    const [img, setimg] = useState("");
+ 
+    const fetchData = () => {
+        fetch(" https://randomuser.me/api/")
+            .then(response => {
+                return response.json()
+            })
+            .then(data => {
+                setUsers(data)
+                console.log(data);
+                setName(data.results[0].name.first+" "+data.results[0].name.last)
+                setimg(data.results[0].picture.large)
+                setState3(false);
+                setState1(false);
+                setState2(false);
+                
+            })
+    }
+    useEffect(() => {
+        fetchData()
+    }, [])
 
-const RANDOM_USER_API = "https://randomuser.me/api/";
-let age = 24;
-let email = "rainajavid15@gmail.com";
-let phone = "(+91) 9797555369";
+    const ageCal = () => {
+        console.log(users.results[0].dob.age);
+        setAge(users.results[0].dob.age)
+        setState1(true);
+        setState2(false);
+        setState3(false);
+    }
+    const emailCal = () => {
+        console.log(users.results[0].email);
+        setEmail(users.results[0].email)
+        setState2(true);
+        setState1(false);
+        setState3(false);
+    };
+    const phoneCal = () => {
+        console.log(users.results[0].phone);
+        setPhone(users.results[0].phone)
+        setState3(true);
+        setState1(false);
+        setState2(false);
+    }
 
-function getRandomUser(){
-     return fetch(RANDOM_USER_API).then(res => res.json()).then(res => res.results[0]);
-}
-
-async function renderRandomUser(){
-     let randomUser = await getRandomUser();
-     imageElement.src = randomUser.picture.large;
-     nameElement.innerText = `${randomUser.name.first} ${randomUser.name.last}`;
-     age = randomUser.dob.age;
-     email = randomUser.email;
-     phone = randomUser.phone;
-     console.log(randomUser);
-}
-
-renderRandomUser();
-
-ageButton.addEventListener('click', _ => {
-     additionalInfoElement.innerText = age;
-});
-emailButton.addEventListener('click', _ => {
-     additionalInfoElement.innerText = email;
-});
-phoneButton.addEventListener('click', _ => {
-     additionalInfoElement.innerText = phone;
-});
-
-getUserButton.addEventListener('click', renderRandomUser);
-  
-  return (
+    return (
     <div id="main">
-      <div class="container">
-          <div class="img-container">
-               <img src="" id="image" alt="img">
-          </div>
-          <h1 id="name">Javid Ahmad Raina</h1>
-          <div class="buttons">
-               <button id="age" data-attr = "age">Age</button>
-               <button id="email" data-attr = "email">Email</button>
-               <button id="phone" data-attr = "phone">Phone</button>
-          </div>
-          <section>
-               <h1 id="additional-info">Additional info</h1>
-          </section>
-          <button id="getUser">Get User</button>
-     </div>
-    </div>
-  )
+    <div>
+      <h1>{name}</h1>
+      <img src={img}/>
+      </div>
+      <button data-attr="age" onClick={ageCal}>Age</button>
+      <button data-attr="email" onClick={emailCal}>Email</button>
+      <button data-attr="phone" onClick={phoneCal}>Phone</button>
+      <section>
+          <h2>Additional Info
+          </h2>
+
+          {state1 && <p id="data">{age}</p>}
+          {state2 && <p id="data">{email}</p>}
+          {state3 && <p id="data">{phone}</p>}
+
+
+      </section>
+      <button id="getUser" onClick={fetchData}>Next</button>
+  </div>
+
+)
 }
 
 
